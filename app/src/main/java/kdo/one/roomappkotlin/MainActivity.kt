74311575516d48c1429d68364aa6c237bfd34784
Abylayyy <abylay.tillabek@gmail.com>
2019-12-23 12:09:39 +0600

    first commit

---
+++ app/src/main/java/kdo/one/roomappkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package kdo.one.roomappkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
